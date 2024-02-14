4afa757c6a07e8daa57ab2084f0cea7cef877b26
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-02-13 21:07:32 -0800

    adding formula

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Homebrew
 Formula for brew
+
+Followed this tutorial: <a href="http://csl.cool/2023/10/27/ios-dev/upload-a-cli-tool-written-in-swift-to-homebrew/">here/</a>
---
+++ compass.rb
@@ -0,0 +1,17 @@
+// compass.rb
+class Localdic < Formula
+  desc "Command Line Interface for Sailor - A Frontend Web Framework in Swift"
+  homepage "https://github.com/SailorWebFramework/Compass"
+  url "https://github.com/SailorWebFramework/Compass", tag: "0.0.1"
+  version "0.0.1"
+
+  depends_on "xcode": [:build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}localdic", "list"
+  end
+end
