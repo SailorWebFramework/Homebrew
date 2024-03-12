2cceac5a234ed1fb4f011295e7b824b1cabdfa6c
Christopher Bannon <54155498+Cbannon35@users.noreply.github.com>
2024-03-12 02:16:49 -0700

    Update compass.rb

--- Formula/compass.rb
+++ Formula/compass.rb
@@ -4,7 +4,7 @@ class Compass < Formula
   url "https://github.com/SailorWebFramework/Compass", tag: "0.0.1"
   version "0.0.1"
 
-  # depends_on "xcode": [:build]
+  depends_on "xcode": [:build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
